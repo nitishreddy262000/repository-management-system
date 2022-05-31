78d03cbc686d57e4c7f0755e1408470fa6f561bc
Nitish <mandlanitish@gmail.com>
2022-05-31 21:52:33 +0530

    upload controller

--- src/main/java/com/repository/repository/controller/MyController.java
+++ src/main/java/com/repository/repository/controller/MyController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapProperties.Credential;
 import org.springframework.cglib.core.CollectionUtils;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.repository.repository.dao.StudentInfoRepository;
 import com.repository.repository.dao.credentialsRepository;
@@ -59,4 +62,9 @@ public class MyController {
 		return studentInfoRepository.findByName(name).get(0);
 	}
 	
+	
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity uploadFile(@RequestParam MultipartFile file) {
+        return ResponseEntity.ok().build();
+    }
 }
