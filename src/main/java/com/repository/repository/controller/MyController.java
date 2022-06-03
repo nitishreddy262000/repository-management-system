698e3dcb16e5104ae0b915c2e248197565beab87
Nitish <mandlanitish@gmail.com>
2022-06-03 20:43:58 +0530

    drive upload

--- src/main/java/com/repository/repository/controller/MyController.java
+++ src/main/java/com/repository/repository/controller/MyController.java
@@ -1,5 +1,7 @@
 package com.repository.repository.controller;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +18,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.google.api.services.drive.model.File;
 import com.repository.repository.dao.StudentInfoRepository;
 import com.repository.repository.dao.credentialsRepository;
 import com.repository.repository.dao.credentialsentity;
 import com.repository.repository.dao.studentinfo;
 import com.repository.repository.entities.MyCredentials;
+import com.spring.service.GoogleDriveService;
+import com.spring.service.imp.GoogleDriveServiceImp;
 
 import antlr.StringUtils;
 
@@ -28,6 +33,8 @@ import antlr.StringUtils;
 @RestController
 public class MyController {
 
+	@Autowired
+	private GoogleDriveServiceImp driveService;
 	@Autowired
 	private StudentInfoRepository studentInfoRepository;
 	@Autowired
@@ -63,8 +70,40 @@ public class MyController {
 	}
 	
 	
-    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity uploadFile(@RequestParam MultipartFile file) {
-        return ResponseEntity.ok().build();
+//    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    public ResponseEntity uploadFile(@RequestParam MultipartFile file) {
+//        return ResponseEntity.ok().build();
+//    }
+//    
+//    @GetMapping("/test")
+//	public void testImageUpload() {
+//		java.io.File file = new java.io.File("C:\\Users\\mandl\\OneDrive\\Pictures\\Screenshots\\image.png");
+//		com.google.api.services.drive.model.File file2  = driveService.upLoadFile(file.getName(), file.getAbsolutePath(),"image/png");
+//		try {
+//			System.err.println(file2.toPrettyString());
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//	}
+    
+    @PostMapping(value = "/upload/file", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity testFolder(@RequestParam MultipartFile file) throws IOException {
+
+    	
+		com.google.api.services.drive.model.File file2  = driveService.upLoadFileNew(convertMultiPartToFile(file),null);
+		try {
+			System.err.println(file2.toPrettyString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return ResponseEntity.ok().build();
+	}
+    
+    private java.io.File convertMultiPartToFile(MultipartFile file ) throws IOException {
+        java.io.File convFile = new java.io.File( file.getOriginalFilename() );
+        FileOutputStream fos = new FileOutputStream( convFile );
+        fos.write( file.getBytes() );
+        fos.close();
+        return convFile;
     }
 }
